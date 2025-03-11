17ec40b3f58119df1b27bf1bd1d18adf0df1ae33
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2025-03-11 16:25:08 -0400

    Tech Debt: Move mongock change unit files to sub package to de-clutter config directory.
    Signed-off-by: Joseph Kotanchik <joseph.kotanchik@semanticbits.com>

---
+++ src/test/java/cms/gov/madie/measure/config/mongock/AddPopulationBasisChangeUnitTest.java
@@ -0,0 +1,38 @@
+package cms.gov.madie.measure.config.mongock;
+
+import cms.gov.madie.measure.repositories.PopulationBasisRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.cms.madie.models.common.PopulationBasis;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.internal.verification.Times;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class AddPopulationBasisChangeUnitTest {
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void addPopulationBasisValues() throws IOException {
+    PopulationBasisRepository populationBasisRepository = mock(PopulationBasisRepository.class);
+    new AddPopulationBasisChangeUnit(new ObjectMapper())
+        .addPopulationBasisValues(populationBasisRepository);
+
+    ArgumentCaptor<List<PopulationBasis>> populationBasisList = ArgumentCaptor.forClass(List.class);
+    verify(populationBasisRepository, new Times(1)).insert(populationBasisList.capture());
+    assertEquals(108, populationBasisList.getValue().size());
+  }
+
+  @Test
+  void rollbackExecution() {
+    PopulationBasisRepository populationBasisRepository = mock(PopulationBasisRepository.class);
+    new AddPopulationBasisChangeUnit(new ObjectMapper())
+        .rollbackExecution(populationBasisRepository);
+    verify(populationBasisRepository, new Times(1)).deleteAll();
+  }
+}
