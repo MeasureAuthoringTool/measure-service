977394fafe93c4c48799086c18c9431f7bb6338f
Rohit Kandimalla <rohit.kandimalla@icf.com>
2025-04-07 12:13:37 -0400

    MAT-8506 Updating the Export exceptions when saved exports are not available in Mongo

--- src/main/java/cms/gov/madie/measure/services/MongoGridFsService.java
+++ src/main/java/cms/gov/madie/measure/services/MongoGridFsService.java
@@ -15,7 +15,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -26,18 +25,17 @@ public class MongoGridFsService {
     if (gridFsId == null || gridFsId.isEmpty()) {
       return null;
     }
-    Optional<GridFSFile> file =
-        Optional.ofNullable(
-            operations.findOne(Query.query(Criteria.where("_id").is(new ObjectId(gridFsId)))));
-    if (file.isPresent()) {
-      GridFsResource resource = operations.getResource(file.get());
-      try (InputStream inputStream = resource.getInputStream()) {
-        return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
-      } catch (IOException e) {
-        throw new MongoGridFSException("Failed to read GridFS content for ID: " + gridFsId, e);
-      }
+    GridFSFile file =
+        operations.findOne(Query.query(Criteria.where("_id").is(new ObjectId(gridFsId))));
+    if (file == null) {
+      return null;
     }
-    return null;
+    GridFsResource resource = operations.getResource(file);
+    try (InputStream inputStream = resource.getInputStream()) {
+      return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new MongoGridFSException("Failed to read GridFS content for ID: " + gridFsId, e);
+    }
   }
 
   public ObjectId save(ByteArrayInputStream inputStream, String filename, String contentType) {
