bc18cded2c82388a12a96db34dc3f920ba7e30a7
sb-cecilialiu <cecilia.liu@icf.com>
2025-04-25 07:14:24 -0500

    MAT-8383 export reports

--- src/main/java/cms/gov/madie/measure/services/ExportService.java
+++ src/main/java/cms/gov/madie/measure/services/ExportService.java
@@ -25,7 +25,6 @@ import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-// import org.dhatim.fastexcel.Workbook;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
