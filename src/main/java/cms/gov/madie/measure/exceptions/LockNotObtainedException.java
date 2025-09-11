c7fb5aa9549a8ef58b2cd73bf7420cf184bd244b
sb-cecilialiu <cecilia.liu@icf.com>
2025-09-11 06:03:49 -0500

    MAT-8918 add default constructor

--- src/main/java/cms/gov/madie/measure/exceptions/LockNotObtainedException.java
+++ src/main/java/cms/gov/madie/measure/exceptions/LockNotObtainedException.java
@@ -4,7 +4,11 @@ public class LockNotObtainedException extends RuntimeException {
 
   private static final long serialVersionUID = 3773507296919206651L;
 
-  private static final String MESSAGE = "IDs: %s, are not able to acquire locks locked by: %s";
+  private static final String MESSAGE = "ID: %s, is not able to acquire lock, locked by: %s";
+
+  public LockNotObtainedException() {
+    super("Lock is not obtained");
+  }
 
   public LockNotObtainedException(String id, String lockedBy) {
     super(String.format(MESSAGE, id, lockedBy));
