fa53f4fd5b6e4a3e3fba2b8939ddc4e63a5f9e63
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2025-09-25 12:20:55 -0400

    MAT-8919: Make lockedBy accessible in the exception so that the caller can determine who has the lock.
    Signed-off-by: Joseph Kotanchik <joseph.kotanchik@semanticbits.com>

--- src/main/java/cms/gov/madie/measure/exceptions/LockNotObtainedException.java
+++ src/main/java/cms/gov/madie/measure/exceptions/LockNotObtainedException.java
@@ -1,10 +1,17 @@
 package cms.gov.madie.measure.exceptions;
 
+import lombok.Getter;
+
+import java.io.Serial;
+
+@Getter
 public class LockNotObtainedException extends RuntimeException {
 
-  private static final long serialVersionUID = 3773507296919206651L;
+  @Serial private static final long serialVersionUID = 3773507296919206651L;
 
   private static final String MESSAGE = "ID: %s, is not able to acquire lock, locked by: %s";
+
+  private String lockedBy;
 
   public LockNotObtainedException() {
     super("Lock is not obtained");
@@ -12,6 +19,7 @@ public class LockNotObtainedException extends RuntimeException {
 
   public LockNotObtainedException(String id, String lockedBy) {
     super(String.format(MESSAGE, id, lockedBy));
+    this.lockedBy = lockedBy;
   }
 
   public LockNotObtainedException(String message) {
