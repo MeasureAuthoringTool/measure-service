f13ba03b2ea25ec22bb1c5cd18b1f3dc09602a74
sb-cecilialiu <cecilia.liu@icf.com>
2025-10-27 10:11:15 -0500

    MAT-8830 delete test measures

--- src/main/java/cms/gov/madie/measure/repositories/ActionLogRepositoryImpl.java
+++ src/main/java/cms/gov/madie/measure/repositories/ActionLogRepositoryImpl.java
@@ -1,15 +1,22 @@
 package cms.gov.madie.measure.repositories;
 
 import com.mongodb.client.result.UpdateResult;
+
+import cms.gov.madie.measure.utils.ActionLogCollectionType;
 import gov.cms.madie.models.common.Action;
+import gov.cms.madie.models.common.ActionLog;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,5 +36,37 @@ public class ActionLogRepositoryImpl implements ActionLogRepository {
             update.push("actions").value(action),
             collection);
     return upsert.getUpsertedId() != null || upsert.getModifiedCount() == 1;
+  }
+
+  @Override
+  public List<ActionLog> findAllActionLogs(Class<?> targetClass) {
+    final String collection = ActionLogCollectionType.getCollectionNameForClazz(targetClass);
+    return mongoTemplate.findAll(ActionLog.class, collection);
+  }
+
+  @Override
+  public Collection<ActionLog> saveAllActionLogs(List<ActionLog> actionLogs, Class<?> targetClass) {
+    final String collection = ActionLogCollectionType.getCollectionNameForClazz(targetClass);
+    return mongoTemplate.insert(actionLogs, collection);
+  }
+
+  @Override
+  @Transactional
+  public void removeActionsByUsers(List<String> users, Class<?> targetClass) {
+    final String collection = ActionLogCollectionType.getCollectionNameForClazz(targetClass);
+    log.debug("Removing Actions performed by users: [{}] from collection: [{}]", users, collection);
+    Query query = new Query(Criteria.where("actions.performedBy").in(users));
+    Update update =
+        new Update().pull("actions", Query.query(Criteria.where("performedBy").in(users)));
+
+    UpdateResult result = mongoTemplate.updateMulti(query, update, collection);
+    log.debug(
+        "removeActionsByUsers: UpdateResult: matchedAcount = "
+            + result.getMatchedCount()
+            + " modifiedCount = "
+            + result.getModifiedCount());
+
+    Query emptyActionsQuery = new Query(Criteria.where("actions").size(0));
+    mongoTemplate.remove(emptyActionsQuery, collection);
   }
 }
