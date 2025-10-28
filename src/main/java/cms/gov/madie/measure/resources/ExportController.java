15d4a8a7c4d9c70ed7314778e9c56416245d1257
Edwin Tan <N119494@icf.com>
2025-10-28 13:03:16 -0700

    MAT-9267: log QDM QRDA test case export

--- src/main/java/cms/gov/madie/measure/resources/ExportController.java
+++ src/main/java/cms/gov/madie/measure/resources/ExportController.java
@@ -128,6 +128,8 @@ public class ExportController {
       @RequestHeader("Authorization") String accessToken) {
 
     final String username = principal.getName();
+    byte[] qrdaPackage;
+
     log.info("User [{}] is attempting to export QRDA for measure [{}]", username, id);
 
     Optional<Measure> measureOptional = measureRepository.findById(id);
@@ -136,13 +138,22 @@ public class ExportController {
       throw new ResourceNotFoundException("Measure", id);
     }
 
-    return ResponseEntity.status(HttpStatus.OK)
+    try {
+      qrdaPackage = exportService.getQRDA(requestDTO, accessToken);
+    } catch (Exception e) {
+      log.error("Failed to export QRDA for measure [{}] by user [{}]", id, username, e);
+      throw e;
+    }
+
+    actionLogService.logAction(id, Measure.class, ActionType.EXPORTED_TESTCASES, username);
+
+    return ResponseEntity.ok()
         .header(
             HttpHeaders.CONTENT_DISPOSITION,
             "attachment;filename=\""
                 + ExportFileNamesUtil.getExportFileName(requestDTO.getMeasure())
                 + ".zip\"")
         .contentType(MediaType.APPLICATION_OCTET_STREAM)
-        .body(exportService.getQRDA(requestDTO, accessToken));
+        .body(qrdaPackage);
   }
 }
