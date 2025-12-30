67ddb7bb79a010472a078bc24786f7a26b3d0bcc
Rohit Kandimalla <rohit.kandimalla@icf.com>
2025-12-30 11:03:00 -0500

    MAT-9384 Added unit tests HumanReadableServiceTest

--- src/test/java/cms/gov/madie/measure/services/HumanReadableServiceTest.java
+++ src/test/java/cms/gov/madie/measure/services/HumanReadableServiceTest.java
@@ -2,6 +2,7 @@ package cms.gov.madie.measure.services;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -116,5 +117,257 @@ public class HumanReadableServiceTest {
     String output =
         humanReadableService.getHumanReadableWithCSS(TEST_MEASURE_ID, TEST_USER, TEST_ACCESS_TOKEN);
     assertThat(output, is(equalTo("valid QDM Human Readable")));
+  }
+
+  @Test
+  void testCompareHtml_IdenticalHtml_NoDifferences() {
+    String html =
+        "<table>"
+            + "<tr><th class='row-header'>Measure Name</th><td class='content-container'>Test Measure</td></tr>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'>Test Description</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(html, html);
+
+    assertThat(result.getOldHtml(), is(equalTo(html)));
+    assertThat(result.getNewHtml(), is(equalTo(html)));
+    assertThat(result.getDifferences().isEmpty(), is(true));
+  }
+
+  @Test
+  void testCompareHtml_SingleFieldDifference() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Measure Name</th><td class='content-container'>Old Measure Name</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Measure Name</th><td class='content-container'>New Measure Name</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    assertThat(result.getDifferences().size(), is(equalTo(1)));
+    var diff = result.getDifferences().get(0);
+    assertThat(diff.getField(), is(equalTo("Measure Name")));
+    // The diff values may contain HTML markup from the diff generator
+    assertThat(diff.getOldValue(), is(not(equalTo(""))));
+    assertThat(diff.getNewValue(), is(not(equalTo(""))));
+  }
+
+  @Test
+  void testCompareHtml_MultipleFieldDifferences() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Measure Name</th><td class='content-container'>Old Name</td></tr>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'>Old Description</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Measure Name</th><td class='content-container'>New Name</td></tr>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'>New Description</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    assertThat(result.getDifferences().size(), is(equalTo(2)));
+    assertThat(
+        result.getDifferences().stream().anyMatch(d -> d.getField().equals("Measure Name")),
+        is(true));
+    assertThat(
+        result.getDifferences().stream().anyMatch(d -> d.getField().equals("Description")),
+        is(true));
+  }
+
+  @Test
+  void testCompareHtml_FieldAddedInNewHtml() {
+    String oldHtml = "<table>" + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>New Field</th><td class='content-container'>New Value</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    assertThat(result.getDifferences().size(), is(equalTo(1)));
+    var diff = result.getDifferences().get(0);
+    assertThat(diff.getField(), is(equalTo("New Field")));
+    assertThat(diff.getOldValue(), is(equalTo("")));
+    assertThat(diff.getNewValue().contains("New Value"), is(true));
+  }
+
+  @Test
+  void testCompareHtml_FieldRemovedInNewHtml() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Old Field</th><td class='content-container'>Old Value</td></tr>"
+            + "</table>";
+    String newHtml = "<table>" + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    assertThat(result.getDifferences().size(), is(equalTo(1)));
+    var diff = result.getDifferences().get(0);
+    assertThat(diff.getField(), is(equalTo("Old Field")));
+    assertThat(diff.getOldValue().contains("Old Value"), is(true));
+    assertThat(diff.getNewValue(), is(equalTo("")));
+  }
+
+  @Test
+  void testCompareHtml_FormattingDifferencesIgnored() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'><b>Bold Text</b></td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'><strong>Bold Text</strong></td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    // Should be no differences since <b> and <strong> are normalized
+    assertThat(result.getDifferences().isEmpty(), is(true));
+  }
+
+  @Test
+  void testCompareHtml_WhitespaceDifferencesIgnored() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'>Text    with   spaces</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'>Text with spaces</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    // Should be no differences since whitespace is normalized
+    assertThat(result.getDifferences().isEmpty(), is(true));
+  }
+
+  @Test
+  void testCompareHtml_MultiValueField_ItemAdded() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Denominator</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    assertThat(result.getDifferences().size(), is(equalTo(1)));
+    var diff = result.getDifferences().get(0);
+    assertThat(diff.getField(), is(equalTo("Population")));
+    assertThat(diff.getOldValue(), is(equalTo("")));
+    assertThat(diff.getNewValue().contains("Denominator"), is(true));
+  }
+
+  @Test
+  void testCompareHtml_MultiValueField_ItemRemoved() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Denominator</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    assertThat(result.getDifferences().size(), is(equalTo(1)));
+    var diff = result.getDifferences().get(0);
+    assertThat(diff.getField(), is(equalTo("Population")));
+    assertThat(diff.getOldValue().contains("Denominator"), is(true));
+    assertThat(diff.getNewValue(), is(equalTo("")));
+  }
+
+  @Test
+  void testCompareHtml_MultiValueField_OrderAgnostic() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Denominator</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Denominator</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    // Should be no differences since order doesn't matter for multi-value fields
+    assertThat(result.getDifferences().isEmpty(), is(true));
+  }
+
+  @Test
+  void testCompareHtml_ComplexHtmlWithMultipleDifferences() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Measure Name</th><td class='content-container'>Old Measure</td></tr>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'>Description text</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Denominator</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Measure Name</th><td class='content-container'>New Measure</td></tr>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container'>Description text</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Initial Population</td></tr>"
+            + "<tr><th class='row-header'>Population</th><td class='content-container'>Numerator</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    // Should have differences for Measure Name change and Population changes
+    assertThat(result.getDifferences().size() >= 2, is(true));
+    assertThat(
+        result.getDifferences().stream().anyMatch(d -> d.getField().equals("Measure Name")),
+        is(true));
+    assertThat(
+        result.getDifferences().stream()
+            .anyMatch(
+                d -> d.getField().equals("Population") && d.getOldValue().contains("Denominator")),
+        is(true));
+    assertThat(
+        result.getDifferences().stream()
+            .anyMatch(
+                d -> d.getField().equals("Population") && d.getNewValue().contains("Numerator")),
+        is(true));
+  }
+
+  @Test
+  void testCompareHtml_EmptyHtmlDocuments() {
+    String oldHtml = "<table></table>";
+    String newHtml = "<table></table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    assertThat(result.getDifferences().isEmpty(), is(true));
+  }
+
+  @Test
+  void testCompareHtml_HtmlWithInlineStyles() {
+    String oldHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container' style='color:red;'>Red Text</td></tr>"
+            + "</table>";
+    String newHtml =
+        "<table>"
+            + "<tr><th class='row-header'>Description</th><td class='content-container' style='color:blue;'>Red Text</td></tr>"
+            + "</table>";
+
+    var result = humanReadableService.compareHtml(oldHtml, newHtml);
+
+    // Inline styles should be ignored in comparison
+    assertThat(result.getDifferences().isEmpty(), is(true));
   }
 }
