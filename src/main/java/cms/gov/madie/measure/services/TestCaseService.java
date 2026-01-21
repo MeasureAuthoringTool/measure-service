2ef5e7d3b0c11d5b3947be4519bedec80faafdb0
Prateek Keerthi <prateekkeerthi7@gmail.com>
2026-01-21 16:56:28 -0500

    MAT-9565 added exception message

--- src/main/java/cms/gov/madie/measure/services/TestCaseService.java
+++ src/main/java/cms/gov/madie/measure/services/TestCaseService.java
@@ -989,7 +989,7 @@ public class TestCaseService {
               TestCaseServiceUtil.parseAndUpdateJsonWithGroupAndTitle(
                   testCase.getJson(), testCase.getSeries(), testCase.getTitle());
           if (updatedJson.get("isUpdated") != null && !(Boolean) updatedJson.get("isUpdated")) {
-            throw new Exception();
+            throw new Exception("Test Case has no family or group.");
           }
           testCase.setJson((String) updatedJson.get("updatedJson"));
           updateTestCase(testCase, measureId, userName, accessToken);
