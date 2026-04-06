35a894e4664cdc699e16858a589d90539e7f54b8
Edwin Tan <N119494@icf.com>
2026-04-06 16:26:17 -0700

    Add bearer token interceptor to NotificationServiceConfig to forward caller JWT and fix 401s on notification-service calls

--- src/main/java/cms/gov/madie/measure/config/NotificationServiceConfig.java
+++ src/main/java/cms/gov/madie/measure/config/NotificationServiceConfig.java
@@ -2,8 +2,16 @@ package cms.gov.madie.measure.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.client.RestTemplate;
 
 @Getter
@@ -15,6 +23,19 @@ public class NotificationServiceConfig {
 
   @Bean
   public RestTemplate notificationServiceRestTemplate() {
-    return new RestTemplate();
+    return new RestTemplateBuilder()
+        .additionalInterceptors(bearerTokenInterceptor())
+        .build();
+  }
+
+  private ClientHttpRequestInterceptor bearerTokenInterceptor() {
+    return (HttpRequest request, byte[] body, ClientHttpRequestExecution execution) -> {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication instanceof JwtAuthenticationToken jwtToken) {
+        String token = jwtToken.getToken().getTokenValue();
+        request.getHeaders().set(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+      }
+      return execution.execute(request, body);
+    };
   }
 }
